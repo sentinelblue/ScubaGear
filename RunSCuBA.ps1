f7fa3a2106967db46301ab05b92b9a6241bd2b93
Ben Wheat <ben@sentinelblue.com>
2022-10-31 15:46:00 -0400

    edit endpoint default

--- RunSCuBA.ps1
+++ RunSCuBA.ps1
@@ -10,7 +10,7 @@ param (
 
 $LogIn        = $true # Set $true to authenticate yourself to a tenant or if you are already authenticated set to $false to avoid reauthentication
 $ProductNames = @("aad", "defender", "exo", "onedrive", "sharepoint", "teams") # The specific products that you want the tool to assess.
-$Endpoint     = "usgov" # Mandatory parameter if running Power Platform. Valid options are "dod", "prod", "preview", "tip1", "tip2", "usgov", or "usgovhigh".
+$Endpoint     = "usgovhigh" # Mandatory parameter if running Power Platform. Valid options are "dod", "prod", "preview", "tip1", "tip2", "usgov", or "usgovhigh".
 $OutPath      = "./Reports" # Report output directory path. Leave as-is if you want the Reports folder to be created in the same directory where the script is executed.
 $OPAPath      = "./" # Path to the OPA Executable. Leave this as-is for most cases.
 $Environment  = "USGov" # Set the M365 Cloud Environment. Options are "Global" or "USGov".
