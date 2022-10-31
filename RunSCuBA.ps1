3efb7af8d40a076620c414212f2d429b3f68a881
Ben Wheat <ben@sentinelblue.com>
2022-10-31 16:17:36 -0400

    Edit example wording

--- RunSCuBA.ps1
+++ RunSCuBA.ps1
@@ -8,7 +8,7 @@ param (
     $Version
 )
 
-<# Defaults for "Global" cloud environment endpoints and "global" endpoints
+<# Defaults for "Global" cloud environment endpoints and "global" M365
 $LogIn            = $true
 $ProductNames     = @("aad", "defender", "exo", "onedrive", "sharepoint", "teams")
 $Endpoint         = "prod"
@@ -17,7 +17,7 @@ $OPAPath          = "./"
 $CloudEnvironment = "Global"
 #>
 
-<# Defaults for "Global" cloud environment endpoints and "GCC" endpoints
+<# Defaults for "Global" cloud environment endpoints and "GCC" M365
 $LogIn            = $true
 $ProductNames     = @("aad", "defender", "exo", "onedrive", "sharepoint", "teams")
 $Endpoint         = "usgov"
@@ -26,7 +26,7 @@ $OPAPath          = "./"
 $CloudEnvironment = "Global"
 #>
 
-<# Defaults for "Azure Government" cloud environment endpoints and "GCC High" endpoints
+<# Defaults for "Azure Government" cloud environment endpoints and "GCC High" M365
 $LogIn            = $true
 $ProductNames     = @("aad", "defender", "exo", "onedrive", "sharepoint", "teams")
 $Endpoint         = "usgovhigh"
@@ -35,12 +35,12 @@ $OPAPath          = "./"
 $CloudEnvironment = "USGovHigh"
 #>
 
-$LogIn        = $true # Set $true to authenticate yourself to a tenant or if you are already authenticated set to $false to avoid reauthentication
-$ProductNames = @("aad", "defender", "exo", "onedrive", "sharepoint", "teams") # The specific products that you want the tool to assess.
-$Endpoint     = "usgovhigh" # Mandatory parameter if running Power Platform. Valid options are "dod", "prod", "preview", "tip1", "tip2", "usgov", or "usgovhigh".
-$OutPath      = "./Reports" # Report output directory path. Leave as-is if you want the Reports folder to be created in the same directory where the script is executed.
-$OPAPath      = "./" # Path to the OPA Executable. Leave this as-is for most cases.
-$CloudEnvironment  = "USGovHigh" # Set the M365 Cloud Environment. Options are "Global" or "USGovHigh".
+$LogIn            = $true # Set $true to authenticate yourself to a tenant or if you are already authenticated set to $false to avoid reauthentication
+$ProductNames     = @("aad", "defender", "exo", "onedrive", "sharepoint", "teams") # The specific products that you want the tool to assess.
+$Endpoint         = "usgovhigh" # Mandatory parameter if running Power Platform. Valid options are "dod", "prod", "preview", "tip1", "tip2", "usgov", or "usgovhigh".
+$OutPath          = "./Reports" # Report output directory path. Leave as-is if you want the Reports folder to be created in the same directory where the script is executed.
+$OPAPath          = "./" # Path to the OPA Executable. Leave this as-is for most cases.
+$CloudEnvironment = "USGovHigh" # Set the M365 Cloud Environment. Options are "Global" or "USGovHigh".
 
 $SCuBAParams = @{
     'Login'            = $Login;
